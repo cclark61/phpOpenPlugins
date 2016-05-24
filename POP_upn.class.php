dc716a897cbb4517eb5c2796b5f9e25ee0b225f6
Christian Clark <cclark61@gmail.com>
2016-05-24 12:18:06 -0400

    UPN Plugin Development.

--- POP_upn.class.php
+++ POP_upn.class.php
@@ -10,7 +10,7 @@
 * @copyright	Copyright (c) Christian J. Clark
 * @license		http://www.gnu.org/licenses/gpl-2.0.txt
 * @link			http://www.emonlade.net/phpopenplugins/
-* @version 		Started: 5-1-2016, Last updated: 5-3-2016
+* @version 		Started: 5-1-2016, Last updated: 5-24-2016
 **/
 //*****************************************************************************
 //*****************************************************************************
@@ -42,13 +42,45 @@ class POP_upn
         // Process Path Parts
         //----------------------------------------------------
         foreach ($path_parts as $part) {
+
+            //----------------------------------------------------
+            // Set Data Element
+            //----------------------------------------------------
             if ($mode == 'set') {
+	            if ($path_parts) {
+		            $recurse = function($subject, $path_parts, $value) use (&$recurse)
+		            {
+			            $pp = array_shift($path_parts);
+			            if (!$path_parts) {
+				            $subject[$pp] = $value;
+			            }
+			            else {
+				            if (!isset($subject[$pp])) { $subject[$pp] = []; }
+				            $subject[$pp] = $recurse($subject[$pp], $path_parts, $value);
+						}
+						return $subject;
+		            };
+					$subject = $recurse($subject, $path_parts, $value);
+		            return true;
+		        }
                 return false;
             }
             //----------------------------------------------------
             // Get Data Element
             //----------------------------------------------------
             else {
+	            if ($path_parts) {
+		            $tmp_subject = &$subject;
+		            foreach ($path_parts as $pp) {
+			            if (isset($tmp_subject[$pp])) {
+				            $tmp_subject = $tmp_subject[$pp];
+			            }
+			            else {
+				            return null;
+			            }
+		            }
+		            return $tmp_subject;
+	            }
                 return null;
             }
         }
@@ -65,9 +97,9 @@ class POP_upn
         // Pull / Set Args
         //----------------------------------------------------
         $num_args = count($args);
-        $args_0 = (!empty($args[0])) ?: (false);
-        $args_1 = (!empty($args[1])) ?: (false);
-        $args_2 = (!empty($args[2])) ?: (false);
+        $args_0 = (!empty($args[0])) ? ($args[0]) : (false);
+        $args_1 = (!empty($args[1])) ? ($args[1]) : (false);
+        $args_2 = (!empty($args[2])) ? ($args[2]) : (false);
 
         //----------------------------------------------------
         // Valid Data Element
@@ -81,13 +113,14 @@ class POP_upn
         //----------------------------------------------------
         // Get Handler
         //----------------------------------------------------
-        $upn_parts = explode(':', $full_upn);
+        $upn_parts = explode(':/', $full_upn);
         if (count($upn_parts) < 2) {
             trigger_error('Invalid UPN Path.');
             return false;
         }
         $handler = $upn_parts[0];
         $mode = false;
+        $value = false;
 
         //----------------------------------------------------
         // Validate Handler
@@ -95,28 +128,32 @@ class POP_upn
         switch ($handler) {
 
             case 'config':
-                $mode = ($num_args > 1) ? ('get') ? ('set');
+                $mode = ($num_args > 1) ? ('set') : ('get');
                 $subject = (isset($_SESSION['config'])) ?: (false);
+                if ($mode == 'set') { $value = $args_1; }
                 break;
 
             case 'json':
-                $mode = ($num_args > 2) ? ('get') ? ('set');
+                $mode = ($num_args > 2) ? ('set') : ('get');
                 $subject = json_decode($args_2);
+                if ($mode == 'set') { $value = $args_2; }
                 break;
 
             case 'array':
-                $mode = ($num_args > 2) ? ('get') ? ('set');
-                $subject = (is_array($args_2)) ?: (false);
+                $mode = ($num_args > 2) ? ('set') : ('get');
+                $subject = (is_array($args_2)) ? ($args_2) : (false);
+                if ($mode == 'set') { $value = $args_2; }
                 break;
 
             case 'session':
             case 'post':
             case 'get':
-            case 'request'
+            case 'request':
             case 'server':
             case 'globals':
-                $mode = ($num_args > 1) ? ('get') ? ('set');
-                switch () {
+                $mode = ($num_args > 1) ? ('set') : ('get');
+                if ($mode == 'set') { $value = $args_1; }
+                switch ($handler) {
                     case 'session':
                         $subject =& $_SESSION;
                         break;
@@ -126,7 +163,7 @@ class POP_upn
                     case 'get':
                         $subject =& $_GET;
                         break;
-                    case 'request'
+                    case 'request':
                         $subject =& $_REQUEST;
                         break;
                     case 'server':
@@ -149,6 +186,13 @@ class POP_upn
         // Get Path Parts
         //----------------------------------------------------
         $path_parts = explode('/', $upn_parts[1]);
+        if ($path_parts[0] == '') {
+	        array_shift($path_parts);
+        }
+        $last_pos = count($path_parts) - 1;
+        if ($path_parts[$last_pos] == '') {
+	        array_pop($path_parts);
+        }
 
         //----------------------------------------------------
         // Return Data
@@ -158,7 +202,8 @@ class POP_upn
             'handler' => $handler,
             'path_parts' => $path_parts,
             'mode' => $mode,
-            'subject' => $subject
+            'subject' => $subject,
+            'value' => $value
         ];
     }
 
--- website_gen_functions.inc.php
+++ website_gen_functions.inc.php
@@ -5,12 +5,12 @@
 * Generic Function Library for Websites
 *
 * @package		phpOpenPlugins
-* @subpackage	Generic Function Library for Websites
+* @subpackage	Global Functions
 * @author 		Christian J. Clark
 * @copyright	Copyright (c) Christian J. Clark
 * @license		http://www.gnu.org/licenses/gpl-2.0.txt
 * @link			http://www.emonlade.net/phpopenplugins/
-* @version 		Started: 3/9/2009, Last updated: 7/17/2012
+* @version 		Started: 3/9/2009, Last updated: 5/24/2016
 **/
 //*****************************************************************************
 //*****************************************************************************
